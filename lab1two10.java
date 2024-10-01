693b0151077dc330daa4a334ab4ec1d2a283b27e
James <90049146+Schlingel15@users.noreply.github.com>
2024-10-01 11:09:25 +0100

    Update lab1two10.java

--- lab1two10.java
+++ lab1two10.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class lab1two10 {
-	// dont forget to add the check
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		long card = sc.nextLong();
