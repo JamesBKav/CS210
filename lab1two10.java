bc61ea165e70e8ce9338977d5b4cfb98af4cb807
James <90049146+Schlingel15@users.noreply.github.com>
2024-10-01 11:09:13 +0100

    Create lab1two10.java

---
+++ lab1two10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class lab1two10 {
+	// dont forget to add the check
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		long card = sc.nextLong();
+		sc.close();
+		int sums = 0, multi = 1;
+		
+		//1
+		while(card > 0) {
+			if(multi == 1) {		// if multi is 1
+				sums += (card%10);
+				multi = 2;
+			}else {					// if multi = 2
+				if((card%10)*2 >=10) {
+					sums += (card%10)*2-9;
+				}else {
+					sums += (card%10)*2;
+				}
+				multi = 1;
+			}
+			card = card/10;
+		}
+		if(sums%10 == 0) {
+			System.out.println("VALID");
+		}else {
+			System.out.println("INVALID");
+		}
+		
+	}
+}
