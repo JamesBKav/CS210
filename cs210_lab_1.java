c62cfaf79ce825628111614a69483af7ae188a48
James <90049146+Schlingel15@users.noreply.github.com>
2024-10-03 19:00:08 +0100

    Update and rename lab1two10.java to cs210_lab_1.java

--- cs210_lab_1.java
+++ cs210_lab_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lab1two10 {
+public class cs210_lab_1 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		long card = sc.nextLong();
