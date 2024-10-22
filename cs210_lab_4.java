e92a8b3aa8e62466a20eeaf0d03f2ad0191685aa
James <90049146+JamesBKav@users.noreply.github.com>
2024-10-22 11:01:02 +0100

    Update cs210_lab_4.java

--- cs210_lab_4.java
+++ cs210_lab_4.java
@@ -1,1 +1,22 @@
-a
+package cs210_lab_4;
+import java.util.*;
+public class cs210_lab_4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// defines array length
+		String[] list = new String[sc.nextInt()+1];
+    // puts values in array
+		for(int i = 0; i < list.length;i++) {
+			list[i] = sc.nextLine();
+		}
+		sc.close();    // closes scanner
+    // prints list
+		for(int i = 0; i < list.length;i++) {
+			System.out.println(list[i]);
+		}
+	}
+  // sorts string
+	static void sorter(String[] s) {
+		//
+	}
+}
